7b19e9de04b95a9f77ea82788492439b3b353df9
Victor Dantas <victorr.dantas@gmail.com>
2024-02-20 15:38:54 -0400

    Add Configuration of Redis with Java

--- src/main/java/com/vct/springbasicauth/config/RedisConfig.java
+++ src/main/java/com/vct/springbasicauth/config/RedisConfig.java
@@ -4,9 +4,14 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 
 import java.time.Duration;
+import java.util.Collections;
 
 import static org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair.fromSerializer;
 
@@ -15,10 +20,31 @@ import static org.springframework.data.redis.serializer.RedisSerializationContex
 public class RedisConfig {
 
     @Bean
+    public RedisConnectionFactory lettuceConnectionFactory() {
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration("localhost", 6379));
+    }
+
+    /*
+    @Bean
     public RedisCacheConfiguration defaultCacheConfiguration() {
         return RedisCacheConfiguration.defaultCacheConfig()
                 .entryTtl(Duration.ofMillis(10000))// 2 horas
                 .disableCachingNullValues()
                 .serializeValuesWith(fromSerializer(new GenericJackson2JsonRedisSerializer()));
     }
+    */
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheConfiguration defaults =  RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMillis(10000))// 2 horas
+                .disableCachingNullValues()
+                .serializeValuesWith(fromSerializer(new GenericJackson2JsonRedisSerializer()));
+
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(defaults)
+                .transactionAware()
+                .build();
+    }
+
+
 }
